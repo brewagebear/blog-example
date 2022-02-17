e889743b3bd70ef73ca10d2de5fcf0aec2237016
dailyworker <seansin@chungbuk.ac.kr>
2022-02-17 21:30:15 +0900

    feat : initialized project

---
+++ spring-producing-ws-example/src/main/java/io/github/dailyworker/springproducingwsexample/SpringProducingWsExampleApplication.java
@@ -0,0 +1,13 @@
+package io.github.dailyworker.springproducingwsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProducingWsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProducingWsExampleApplication.class, args);
+    }
+
+}
---
+++ spring-producing-ws-example/src/test/java/io/github/dailyworker/springproducingwsexample/SpringProducingWsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dailyworker.springproducingwsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProducingWsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
