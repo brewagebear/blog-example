26e7cf6fc8b0780b53dfecfd0e1c162c5ad34a97
신수웅 <sinsuung@wemakeprice.com>
2022-10-23 16:42:59 +0900

    Lightsail CI/CD 예제 추가

---
+++ lightsail-ci-cd-example/ligthsail-example/src/main/java/io/github/brewagebear/LigthsailApp.java
@@ -0,0 +1,13 @@
+package io.github.brewagebear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigthsailApp
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(LigthsailApp.class, args);
+    }
+}
